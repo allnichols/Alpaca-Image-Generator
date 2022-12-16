cc491942ea3f654703ec5aa5d221113770bc1ab6
Max Nichols <allenmaxnichols@gmail.com>
2022-12-16 12:32:45 -0600

    removed unused imports

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
 import userEvent from "@testing-library/user-event";
 
